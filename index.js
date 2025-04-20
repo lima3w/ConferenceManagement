b08fb5131175d7562ac3cdfbd7f725214401fb7e
zack-lewis <1774764+lima3w@users.noreply.github.com>
2025-04-19 21:43:07 -0500

    Added CORS

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 const express = require("express");
+// import cors from "cors";
+const cors = require("cors");
 const app = express();
+
 const port = 3000;
 app.use(express.json());
 const apiV1Router = require("./routes/api-v1");
+app.use(cors());
+// app.use(cors({
+//   origin: 'https://your-frontend-domain.com',
+//   credentials: true, // if using cookies or auth headers
+// }));
 
 // Middleware logging
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path}`);
-  next();
+    console.log(`${req.method} ${req.path}`);
+    next();
 });
 
 app.use("/api/v1", apiV1Router);
 
 app.all("/api/", (req, res) => {
-  res.redirect(301, "/api/v1");
+    res.redirect(301, "/api/v1");
 });
 
 // Root route
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
