dfe6e56ff8f39f3580375be6f531d98834f41000
zack-lewis <1774764+lima3w@users.noreply.github.com>
2025-04-19 22:49:07 -0500

    Formatting

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // GET /sessions/
-router.get('/', (req, res) => {
-  res.send('List of sessions');
+router.get("/", (req, res) => {
+    res.send("List of sessions");
 });
 
 // GET /sessions/:id
-router.get('/:id', (req, res) => {
-  res.send(`session ID: ${req.params.id}`);
+router.get("/:id", (req, res) => {
+    res.send(`session ID: ${req.params.id}`);
 });
 
 // POST /sessions/
-router.post('/', (req, res) => {
-  res.send('Create a new session');
+router.post("/", (req, res) => {
+    res.send("Create a new session");
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,36 +4,40 @@ const Users = require("../functions/users");
 
 // GET /users/
 router.get("/", async (req, res) => {
-  try {
-    res.status(200).json(await Users.getUsers());
-  } catch (err) {
-    res
-      .status(500)
-      .json({ error: "Failed to retrieve users", details: err.message });
-  }
+    try {
+        res.status(200).json(await Users.getUsers());
+    } catch (err) {
+        res.status(500).json({
+            error: "Failed to retrieve users",
+            details: err.message,
+        });
+    }
 });
 
 // GET /users/:id
 router.get("/:id", async (req, res) => {
-  try {
-    const user = await Users.getUserById(req.params.id);
-    if (!user) return res.status(404).json({ error: "User not found" });
-    res.status(200).json(user);
-  } catch (err) {
-    res
-      .status(500)
-      .json({ error: "Failed to retrieve user by ID", details: err.message });
-  }
+    try {
+        const user = await Users.getUserById(req.params.id);
+        if (!user) return res.status(404).json({ error: "User not found" });
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json({
+            error: "Failed to retrieve user by ID",
+            details: err.message,
+        });
+    }
 });
 
 // POST /users/
 router.post("/", (req, res) => {
-  if (!req.body || Object.keys(req.body).length === 0) {
-    console.log(req.body);
-    return res.status(400).json({ error: "Request body is missing or empty" });
-  }
+    if (!req.body || Object.keys(req.body).length === 0) {
+        console.log(req.body);
+        return res
+            .status(400)
+            .json({ error: "Request body is missing or empty" });
+    }
 
-  res.send(Users.createUser(req.body));
+    res.send(Users.createUser(req.body));
 });
 
 module.exports = router;
